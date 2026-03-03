1ac6f8c859f48f6c613e6b04e5e14ed9ea2c2d5e
Ambady Dileep <ambadydileep.pro@gmail.com>
2026-03-03 23:44:46 +0530

    Add cross-field validation to validator form using clean()

--- validator/forms.py
+++ validator/forms.py
@@ -7,14 +7,66 @@ class NameValidationForm(forms.Form):
     max_length=20,
     required=True
   )
+  email = forms.EmailField(
+    required=True
+  )
+  password = forms.CharField(
+    min_length=8,
+    required=True
+  )
 
   def clean_name(self):
     name = self.cleaned_data["name"]
 
     if not name.isalpha():
       raise forms.ValidationError("Only letters allowed")
-    
+
     if re.search(r'[aeiou]{3}', name.lower()):
       raise forms.ValidationError("Too many consecutive vowels")
 
-    return name 
+    return name
+
+  def clean_email(self):
+    email = self.cleaned_data['email']
+
+    if email.endswith("@tempmail.com"):
+      raise forms.ValidationError("Temporary emails not allowed")
+
+    return email
+
+  def clean_password(self):
+    password = self.cleaned_data['password']
+
+    if not re.search(r"[A-Z]", password):
+      raise forms.ValidationError("Password should contain an uppercase letter")
+
+    if not re.search(r"\d", password):
+      raise forms.ValidationError("Password must contain a number")
+
+    return password
+
+  def clean(self):
+        cleaned_data = super().clean()
+
+        name = cleaned_data.get("name")
+        password = cleaned_data.get("password")
+        email = cleaned_data.get("email")
+
+        if not name or not password or not email:
+            return cleaned_data  
+
+        # ❌ password should not contain the name
+        if name.lower() in password.lower():
+            raise forms.ValidationError(
+                "Password should not contain your name"
+            )
+
+        # ❌ email username same as name
+        email_username = email.split("@")[0]
+        if email_username.lower() == name.lower():
+            raise forms.ValidationError(
+                "Email username should not be the same as name"
+            )
+
+        return cleaned_data
+
